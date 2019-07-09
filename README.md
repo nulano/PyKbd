fa9592abafb20565318171d09c6e7fe0bf016b3b
nulano <nulano@nulano.eu>
2019-07-09 11:27:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PyKbd
+
+This is a cross-platform Python library for manipulating keyboard layouts.
+
+## License
+
+PyKbd is currently licensed under the GNU AGPLv3 license.
+
+The current plan is to switch to a less restrictive open-source license once this project is more mature.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyKbd',
+    version='0.1.0',
+    description='a cross-platform Python library for creating and manipulating keyboard layouts',
+    packages=['PyKbd'],
+    package_dir={'PyKbd': 'src/PyKbd'},
+    author='Nulano',
+    author_email='nulano@nulano.eu',
+    license='GNU APGLv3 or later'
+)
